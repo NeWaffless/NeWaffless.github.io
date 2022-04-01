4d2d3b8fa87981135f0abcc0130af007da52cb89
Aidan <newaffless@gmail.com>
2022-04-01 17:52:04 +1100

    converted projected back to hmtl, css, js. replaced header and footer with sidebar

--- js/modal-content.js
+++
@@ -1,9 +0,0 @@
-$("#project-modal").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget);
-  var project = button.data("project");
-  var htmlRef = button.data("htmlref");
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this);
-  modal.find(".modal-title").text(project);
-  //   modal.find(".modal-body").text("This should change the modal content");
-});
